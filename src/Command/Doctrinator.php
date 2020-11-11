f8c991aec65e7ebeae61ba94fb183eec54058815
Christopher Kulks <christopher.kulks@insitu.de>
2020-11-11 16:18:54 +0100

    console programm now safely creates a file for each class inside a file that extends Insitu_Instance, also goes through each file; safely checks a given or installed ignore.yaml

--- src/Command/Doctrinator.php
+++ src/Command/Doctrinator.php
@@ -2,7 +2,6 @@
 namespace App\Command;
 
 use PhpParser\Node;
-use PhpParser\PrettyPrinter;
 use PhpParser\Error;
 use PhpParser\NodeDumper;
 use PhpParser\NodeFinder;
@@ -21,10 +20,18 @@ use Symfony\Component\Finder\Iterator\RecursiveDirectoryIterator;
 use App\Helpers\doctrineHelper;
 use App\Helpers\logHelper;
 use App\Helpers\outputHelper;
+use Symfony\Component\Yaml\Yaml;
 
 class Doctrinator extends Command
 {
     protected static $defaultName = 'app:doctrinator';
+    protected $ignoreFilepath = null;
+    protected $logFilepath = null;
+    protected $destinationDirectory = null;
+
+    protected $filesystem = null;
+
+
     protected function configure(){
         $this
             ->setDescription('Creates Doctrine Instances of your Codeigniter Instances.')
@@ -34,99 +41,98 @@ class Doctrinator extends Command
             ->addArgument('destinationDirectory', InputArgument::OPTIONAL, 'The directory path that will contain doctrine entities.');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $outputter = new outputHelper();
-        $logger = new logHelper();
-        $doctriner = new doctrineHelper();
-        $formatter = $this->getHelper('formatter');
-
-        $filesystem = new Filesystem();
-        $ignoreInstallFilepath = sys_get_temp_dir().'/'.'ignore.yaml';
-
-        // Required File Pointers
-        $logFile = null;
-        $ignoreFile = null;
-
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @param FormatterHelper $formatter
+     * @param outputHelper $outputter
+     * @return int
+     */
+    protected function handleArguments(InputInterface $input, OutputInterface $output, FormatterHelper $formatter, outputHelper $outputter) {
+        // TODO check if destination and source are the same -> can't be! because of same file names
         // check of install is required
         // fill ignoreFile with the working ignore file pointer with readonly
         if ($input->getOption('install')) {
-            return $this->install($output, $outputter, $filesystem, $ignoreInstallFilepath, $formatter);
+            return $this->install($output, $outputter, $formatter);
         }
-
-        // check if a working ignore file is given
+        // check if the ignore path is given, and if the path works, then fill the FP
         if ($input->getOption('ignore')) {
-            if(!$filesystem->exists($input->getOption('ignore'))) {
+            // check if the user given path works
+            if(!$this->filesystem->exists($input->getOption('ignore'))) {
                 $outputter->outputError('Ignore path is incorrect, please use --install or -i or change the path to an absolute path.', $output, $formatter);
                 return Command::FAILURE;
             }
-
-            $ignoreFile = fopen($input->getOption('ignore'), 'r');
+            // overwrite the current ignoreFilepath
+            $this->ignoreFilepath = $input->getOption('ignore');
         } else {
-            if(!$filesystem->exists($ignoreInstallFilepath)){
+            // check if the dev set path works
+            if(!$this->filesystem->exists($this->ignoreFilepath)){
                 $outputter->outputError('Ignore file not given, please use --install or -i.', $output, $formatter);
                 return Command::FAILURE;
             }
-
-            $ignoreFile = fopen($ignoreInstallFilepath, 'r');
         }
-
-        // if not tell user to install or create a working ignore file
-        if (!$ignoreFile) {
-            if($input->getOption('ignore')) {
-                $outputter->outputError('Ignore path is incorrect, please use --install or -i or cchange the path to an absolute path.', $output, $formatter);
-            } else {
-                $outputter->outputError('Ignore file not given, please use --install or -i.', $output, $formatter);
-            }
+        if (filesize($this->ignoreFilepath) == 0) {
+            $outputter->outputError('', $output, $formatter);
             return Command::FAILURE;
         }
-
-        // TODO check if ignore File empty output prompt that asks if continue despite empty
-
         if (!$input->getArgument('sourceDirectory')) {
             $outputter->outputError('Source directory as an argument is missing.', $output, $formatter);
             return Command::FAILURE;
         }
-
         if (!$input->getArgument('destinationDirectory')) {
             $outputter->outputError('Destination directory as an argument is missing.', $output, $formatter);
             return Command::FAILURE;
         }
 
-        if ($this->crawl($input->getArgument('sourceDirectory'), $output, $outputter, $formatter) == Command::SUCCESS) {
-            // Do stuff
-            return Command::SUCCESS;
+        // need the replace because of path/sub/ can be the same as path/sub but as strings it's seen as different
+        if(str_replace("/", "", $input->getArgument('sourceDirectory')) == str_replace("/", "",$input->getArgument('destinationDirectory'))) {
+            $outputter->outputError('Destination directory is the same as the source directory, please choose another folder.', $output, $formatter);
+            return Command::FAILURE;
         }
 
-        // log stuff in error log
-        $outputter->outputError('An unexpected error occured.', $output, $formatter);
-        return Command::FAILURE;
+        $this->destinationDirectory = $input->getArgument('destinationDirectory');
     }
 
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** Setting up */
+        $outputter = new outputHelper();
+        $logger = new logHelper();
+        $doctriner = new doctrineHelper();
+        $this->filesystem = new Filesystem();
+        $formatter = $this->getHelper('formatter');
+        $this->ignoreFilepath = sys_get_temp_dir().'/'.'ignore.yaml';
+        /** ---------  */
+
+        if ($this->handleArguments($input, $output, $formatter, $outputter) === Command::FAILURE) {
+            return Command::FAILURE;
+        };
+
+        return $this->crawl($input->getArgument('sourceDirectory'), $output, $outputter, $formatter, $doctriner);
+    }
 
     /**
      * creates the needed files to run the cli
      * @param OutputInterface $output
      * @param outputHelper $outputter
-     * @param Filesystem $filesystem
-     * @param string $filepath
      * @param FormatterHelper $formatter
      * @return int
      */
-    private function install(OutputInterface $output, outputHelper $outputter, Filesystem $filesystem, string $filepath, FormatterHelper $formatter)
+    private function install(OutputInterface $output, outputHelper $outputter, FormatterHelper $formatter)
     {
         $outputter->outputInfo('installing...', $output, $formatter);
 
-        // TODO add prompt that tells you that one already exists at /tmp/ignore.yaml
-        // TODO put Insitu_Instance and Collection near the files
+        if ($this->filesystem->exists($this->ignoreFilepath)) {
+            $outputter->outputInfo('The ignore.yaml already exists under: '. $this->ignoreFilepath, $output, $formatter);
+            return Command::SUCCESS;
+        }
         try {
-            $filesystem->dumpFile($filepath, '# The ignore file for instances to be ignored.');
+            $this->filesystem->dumpFile($this->ignoreFilepath, '# The ignore file for instances to be ignored.');
         } catch (IOExceptionInterface $exception) {
             $outputter->outputError('Failed creating the ignore file at '. $exception->getPath(), $output, $formatter);
             return Command::FAILURE;
         }
-        $outputter->outputInfo('Done! You can find your ignore.yaml under: '. $filepath, $output, $formatter);
-
+        $outputter->outputInfo('Done! You can find your ignore.yaml under: '. $this->ignoreFilepath, $output, $formatter);
         return Command::SUCCESS;
     }
 
@@ -136,60 +142,142 @@ class Doctrinator extends Command
      * @param OutputInterface $output
      * @param outputHelper $outputter
      * @param FormatterHelper $formatter
+     * @param doctrineHelper $doctrineHelper
+     * @return int
      */
-    private function crawl(string $path, OutputInterface $output, outputHelper $outputter, FormatterHelper $formatter)
+    private function crawl(string $path, OutputInterface $output, outputHelper $outputter, FormatterHelper $formatter, doctrineHelper $doctrineHelper)
     {
         $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, 0));
-
-        // $traverser = new NodeTraverser();
-
-        // is for traversing the AST, TypesConverter looks at the Property
-        // $traverser->addVisitor(new TypesConverter());
-
         $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
         $nodeFinder = new NodeFinder();
-        $prettyPrinter = new PrettyPrinter\Standard;
+
+        // parse ignore.yaml
+        $ignoreYaml = Yaml::parse(file_get_contents($this->ignoreFilepath));
+        // $output->writeln(Yaml::dump($ignoreYaml['Instances'][0]));
+        $filesToIgnore = $ignoreYaml['Instances'];
 
         /** @var SplFileInfo  $file */
         foreach ($rii as $file) {
+
+            /** FILE CHECKS */
             if ($file->isDir()) {
                 continue;
             }
-
-            // if the current file is not an instance by name skip
+            // if the current file is not php skip
             if ($file->getExtension() != 'php') {
                 continue;
             }
-
-            // check if the filename is a base insitu instance
-            if (str_contains(strtolower($file->getFilename()), 'insitu_')) {
+            // check if the filename is a base insitu instance skip
+            if (str_contains(strtolower($file->getFilename()), 'insitu_')
+                || $filesToIgnore && in_array($file->getFilename(), $filesToIgnore)) {
                 continue;
             }
+            /** ------------ */
 
             $code = file_get_contents($file->getPathname());
+
             try {
                 $ast = $parser->parse($code);
             } catch (Error $e){
-                $outputter->outputError($e->getMessage(), $output, $formatter);
+                $outputter->outputError($e->getMessage() . ' for ' . $file->getFilename(), $output, $formatter);
                 continue;
             }
 
-            $properties = $nodeFinder->findInstanceOf($ast, Node\Stmt\Property::class);
-            $propProperties = $nodeFinder->find($properties, function (Node $node) {
-                return $node instanceof Node\Stmt\PropertyProperty;
+            /**   Filter the ast with the nodeFinder */
+            // all classes that extend Insitu_Instance
+            $extendingClasses = $nodeFinder->find($ast, function (Node $node) {
+                return $node instanceof Node\Stmt\Class_
+                        && $node->extends !== null
+                        && in_array('Insitu_Instance', $node->extends->parts);
             });
+            // TODO split into insitu_instance extenders and others, try to find the files that are extended if not log!
 
-            $types = $nodeFinder->find($propProperties, function (Node $node) {
-                return $node instanceof Node\Expr\Array_;
-            });
+            if (count($extendingClasses) > 1) {
+                // TODO got several instances of Insitu_Instance in one file -> split into two files, log it also fill entity meta!
+                $outputter->outputInfo('got several instances', $output, $formatter);
+            } else if(count($extendingClasses) === 0) {
+                $outputter->outputInfo('no instance', $output, $formatter);
+                continue;
+            }
 
-            // TODO log if no types given
+            $entitiesMetaObject = [];
 
-            // $dumper = new NodeDumper();
-            // echo $dumper->dump($types) . "\n";
+            foreach($extendingClasses as $extendingClass) {
 
-            echo $prettyPrinter->prettyPrint($types);
-            break;
+                if (count($extendingClasses) > 1) {
+                    // TODO log
+                    $outputter->outputInfo('got several instances', $output, $formatter);
+                }
+
+                /** _types */
+                $types = $nodeFinder->find($extendingClass, function (Node $node) {
+                    return $node instanceof Node\Stmt\PropertyProperty && $node->name == '_types';
+                });
+
+                if (count($types) === 0) {
+                    // TODO log if no types given
+                    $outputter->outputInfo('no types', $output, $formatter);
+                    continue;
+                }
+
+                /** Extracts the types keys and values into a php readable object */
+                $typesObj = [];
+                foreach ($types[0]->default->items as $type) {
+                    $typesObj[$type->key->value] = $type->value->value;
+                }
+
+                /** entity metadata */
+                $table = $nodeFinder->find($extendingClass, function (Node $node) {
+                    return $node instanceof Node\Stmt\PropertyProperty && $node->name == '_table';
+                });
+
+                $entitiesMetaObject[$extendingClass->name->name] = [
+                    'destinationDirectory' => $this->destinationDirectory,
+                    'name' => $extendingClass->name->name,
+                ];
+
+                if (count($table) !== 0) {
+                    $entitiesMetaObject[$extendingClass->name->name] = [
+                        'destinationDirectory' => $this->destinationDirectory,
+                        'name' => $extendingClass->name->name,
+                        'table' => $table[0]->default->value
+                    ];
+                }
+
+                /** functions */
+                // TODO probably filter _construct out of it
+                $classMethods = $nodeFinder->find($extendingClass, function (Node $node) {
+                    return $node instanceof Node\Stmt\ClassMethod;
+                });
+
+                if (count($classMethods) === 0) {
+                    // TODO log if no class methods given
+                    $outputter->outputInfo('no class methods', $output, $formatter);
+                    continue;
+                }
+
+                //TODO foreach for every entity inside the entityMetaObject
+                $entityString = $doctrineHelper->createEntityFileString($entitiesMetaObject[$extendingClass->name->name], $typesObj, $classMethods);
+                try {
+                    $filename = $this->destinationDirectory . '/' . $extendingClass->name->name . '.php';
+                    $outputter->outputInfo('Creating file at ' . $filename, $output, $formatter);
+                    $this->filesystem->dumpFile($filename , $entityString);
+                } catch (IOExceptionInterface $exception) {
+                    $outputter->outputError('Failed creating the entity file at ' . $exception->getPath(), $output, $formatter);
+                    return Command::FAILURE;
+                }
+            }
         }
+
+//             $dumper = new NodeDumper();
+//             echo $dumper->dump($ast) . "\n";
+
+//            echo $prettyPrinter->prettyPrint($types);
+//            $output->writeln('');
+//            echo $prettyPrinter->prettyPrint($classMethods);
+
+            // TODO if function is called but the name is not inside the instance log!
+            // TODO delete break
+        return Command::SUCCESS;
     }
 }
--- src/Helpers/doctrineHelper.php
+++ src/Helpers/doctrineHelper.php
@@ -1,10 +1,116 @@
 <?php
 namespace App\Helpers;
 
+use PhpParser\PrettyPrinter;
+
 class doctrineHelper
 {
-    public function createEntity ()
-    {
+    /**
+     * @param array $entityMetaObject
+     * @param array $propertyObject
+     * @param array $methodsAST
+     * @return string
+     */
+    public function createEntityFileString (array $entityMetaObject, array $propertyObject, array $methodsAST) {
+        $entityString = '';
 
+        !$entityMetaObject['table']
+            ? $entityString .= $this->createEntityHeader($entityMetaObject['destinationDirectory'], $entityMetaObject['name'], $entityMetaObject['table'])
+            : $entityString .= $this->createEntityHeader($entityMetaObject['destinationDirectory'], $entityMetaObject['name']);
+
+
+        foreach ($propertyObject as $propertyKey => $propertyValue) {
+            $entityString .= $this->createProperty($propertyKey, $propertyValue);
+        }
+
+        $entityString .= "\n";
+
+        $prettyPrinter = new PrettyPrinter\Standard;
+        $methodStrings = explode("\n", $prettyPrinter->prettyPrint($methodsAST));
+        foreach ($methodStrings as $methodString) {
+            $entityString .= "\t" . $methodString . "\n";
+        }
+
+        return $entityString . '}' ."\n";
+    }
+
+    /**
+     * @param string $destinationFilepath
+     * @param string $entityName
+     * @param string|null $table
+     * @return string
+     */
+    public function createEntityHeader (string $destinationFilepath, string $entityName, string $table = null) {
+        $entityHeader = '<?php' . "\n";
+
+        if (!$table) {
+            $entityHeader .=
+                '// ' . $destinationFilepath . "\n"
+                . 'use Doctrine\ORM\Mapping as ORM;' . "\n"
+                . '/**' . "\n"
+                . '* @ORM\Entity' . "\n"
+                . '*/' . "\n"
+                . 'class ' . $entityName . ' {' . "\n";
+
+            return $entityHeader;
+        }
+
+        $entityHeader .=
+            '// ' . $destinationFilepath . "\n"
+            . 'use Doctrine\ORM\Mapping as ORM;' . "\n"
+            . '/**' . "\n"
+            . '* @ORM\Entity' . "\n"
+            . '* @ORM\Table(name="' . $table . '")' . "\n"
+            . '*/' . "\n"
+            . 'class ' . $entityName . ' { ' ."\n";
+
+        return $entityHeader;
+
+    }
+
+    /**
+     * @param string $propertyName
+     * @param string $propertyType
+     * @return string
+     */
+    private function createProperty (string $propertyName, string $propertyType) {
+        $propertyString = '';
+        $annotation = $this->createPropertyAnnotation($propertyName, $propertyType);
+        $propertyString .= $annotation
+            . "\t" . 'protected $' . $propertyName . ';' . "\n";
+
+        return $propertyString ."\n";
+    }
+
+    /**
+     * @param string $propertyName
+     * @param string $propertyType
+     * @return string
+     */
+    private function createPropertyAnnotation (string $propertyName, string $propertyType) {
+       // TODO maybe rely on a doctrineMapper.yaml
+       $annotation = "\t" . '/**' . "\n";
+
+        switch ($propertyName) {
+            case 'id':
+                $annotation .=
+                    "\t" .   ' * @ORM\Id' . "\n"
+                    . "\t" . ' * @ORM\GeneratedValue' . "\n";
+
+        }
+
+        switch ($propertyType) {
+            case 'id':
+                $annotation .= "\t" . ' * @ORM\Column(type="integer")' . "\n";
+            break;
+            default:
+                $annotation .= "\t" . ' * @ORM\Column(type="' . $propertyType . '")' . "\n";
+        }
+
+        $annotation .=
+            "\t" .   ' * @var ' . $propertyName . "\n"
+            . "\t" . ' */';
+
+        return $annotation . "\n";
     }
 }
